4f6fdf28b4d2d97362252b8ae36e964f444958a4
Joseph Daniels <joedaniels29@me.com>
2016-07-26 21:19:38 -0400

    typo :(

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ module.exports = {
   },
   included : function(app) {
     this._super.included(app);
-    app.import(app.bowerDirectory'/letteringjs/jquery.lettering.js');
+    app.import(app.bowerDirectory +  '/letteringjs/jquery.lettering.js');
   }
 };
