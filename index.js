f26bc7322a5d55cba5aec281e5f9801c90c686f1
Piotr Bakalarski <piotrb5e3@gmail.com>
2016-10-05 12:22:37 +0200

    Fixed component not imported error.
    Moved letteringjs from dev deps to regular deps

--- index.js
+++ index.js
@@ -4,7 +4,12 @@
 module.exports = {
   name: 'ember-cli-lettering-js',
   included: function (app) {
+    // see: https://github.com/ember-cli/ember-cli/issues/3718
+    if (typeof app.import !== 'function' && app.app) {
+      app = app.app;
+    }
     this._super.included(app);
+
     app.import(app.bowerDirectory + '/letteringjs/jquery.lettering.js');
   }
 };
