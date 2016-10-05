38a5ed9c573fc7043877f34f9044441657147e2e
Piotr Bakalarski <piotrb5e3@gmail.com>
2016-10-05 11:50:54 +0200

    Moved afterInstall hook operations to the default blueprint

--- index.js
+++ index.js
@@ -3,11 +3,8 @@
 
 module.exports = {
   name: 'ember-cli-lettering-js',
-  afterInstall: function () {
-    return this.addBowerPackageToProject("letteringjs");
-  },
-  included: function (app, parentAddon) {
-    var target = (parentAddon || app);
-    target.import('bower_components/letteringjs/jquery.lettering.js');
+  included: function (app) {
+    this._super.included(app);
+    app.import(app.bowerDirectory + '/letteringjs/jquery.lettering.js');
   }
 };
